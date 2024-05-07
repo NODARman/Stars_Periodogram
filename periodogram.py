ad0eb4aae380d07f1bea6f93696a141ff66a5282
NODARman <81517067+NODARman@users.noreply.github.com>
2024-05-08 00:04:43 +0400

    Update periodogram.py

--- periodogram.py
+++ periodogram.py
@@ -1,24 +1,16 @@
-from astropy.timeseries import BoxLeastSquares
-from astropy.timeseries import LombScargle
-from astropy.stats import LombScargle
-from scipy.signal import find_peaks
 import matplotlib.pyplot as plt
-from ast import literal_eval
-import astropy.units as u
 import lightkurve as lk
-import pandas as pd
 import numpy as np
 
 # Set the star identifier
 myStar = "KIC 004041342"
 
-def Periodogram(star_name=myStar, author="Kepler", quarter=10):
+def Periodogram(star_name=myStar):
   # Search and download the light curve data
-  lc = lk.search_lightcurve(star_name, author=author, quarter=quarter).download()
-
   search_result = lk.search_lightcurve(star_name, author="Kepler", cadence="long")
   lc_collection = search_result.download_all()
-
+  
+  # Stitch
   lc_stitched = lc_collection.stitch()
 
   # Plot the periodogram
@@ -27,6 +19,7 @@ def Periodogram(star_name=myStar, author="Kepler", quarter=10):
   Power = Per.power.value
   MaxPeriod = str(round(Per.period_at_max_power.value, 2))
 
+  # Remove unwanted data for visualization
   for i in range(80):
     Period = np.delete(Period, 0)
     Power = np.delete(Power, 0)
